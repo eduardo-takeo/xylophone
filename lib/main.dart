5c5e6abfadeab7e8e79e179d5386c3410aebf257
Eduardo <eduardo.takeo96@gmail.com>
2020-07-05 12:27:07 -0300

    added keys and sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  final player = AudioCache();
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget Key(int note, Color color) {
+    return Expanded(
+      // ignore: missing_required_param
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(note);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                  onPressed: () {
-                    player.play('note1.wav');
-                  },
-                  child: Container(
-                    color: Colors.red,
-                    child: Text('Click here for sound'),
-                  ))
+              Key(1, Colors.red),
+              Key(2, Colors.orange),
+              Key(3, Colors.yellow),
+              Key(4, Colors.green),
+              Key(5, Colors.teal),
+              Key(6, Colors.blue),
+              Key(7, Colors.purple),
             ],
           ),
         ),
